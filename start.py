9cdf476f8f84d267ba260494deeac8ea8f844398
Jeremy Kneebone <jezknee@gmail.com>
2025-07-21 11:13:42 +0100

    v0.2 look at metadata csv

---
+++ start.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import pandas as pd
+pd.set_option("display.max_columns", None)
+#pd.set_option("low_memory",False)
+
+path = Path.cwd() / "Data" / "MetObjects.txt"
+print(path)
+metadata = []
+df = pd.read_csv(path, encoding="utf-8")
+print(df.head())
