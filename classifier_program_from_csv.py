c9ef458b7369c0327ed8bef981313de05be1115c
Jeremy Kneebone <jezknee@gmail.com>
2025-07-31 11:41:32 +0100

    v2.5 feature selection image data only

--- classifier_program_from_csv.py
+++ classifier_program_from_csv.py
@@ -6,6 +6,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from numpy import set_printoptions 
+from sklearn.feature_selection import SelectKBest 
+from sklearn.feature_selection import chi2 
 
 from pandas import read_csv 
 from matplotlib import pyplot 
@@ -20,6 +23,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
 pd.set_option("display.max_columns", None)
+
+
 
 merged = pd.read_csv(Path.cwd() / "Data" / "MetObjWithImageColoursMetadata.csv", encoding="utf-8")
 merged_df = pd.DataFrame(merged)
@@ -48,6 +53,7 @@ century_counts = df["Century"].value_counts()
 print("Century Counts:")
 print(century_counts)
 
+
 names = df.columns.tolist()
 array = df.values
 X = array[:, 2:-1]  # Features (excluding 'Object ID' and 'Century')
@@ -55,6 +61,38 @@ Y = array[:, -1]    # Target variable (last column, 'Century')
 test_size = 0.1
 seed = 7
 
+# feature extraction 
+test = SelectKBest(score_func=chi2, k=10) 
+fit = test.fit(X, Y) 
+# summarize scores 
+set_printoptions(precision=3) 
+print(fit.scores_) 
+features = fit.transform(X) 
+# summarize selected features 
+print(features[0:10,:]) 
+
+feature_names = names[2:-1]  # This matches your X array slice
+
+# Get which features were selected
+selected_mask = test.get_support()
+
+# Get the names of selected features
+selected_features = [feature_names[i] for i in range(len(feature_names)) if selected_mask[i]]
+feature_list = [feature for feature in selected_features]
+print("Selected Features:")
+for i, feature in enumerate(selected_features):
+    print(f"{i+1}. {feature}")
+
+df = df.filter(items=["Object ID"] + selected_features + ["Century"], axis=1)
+print(df.head())
+print(df.columns)
+
+names = df.columns.tolist()
+array = df.values
+X = array[:, 2:-1]  # Features (excluding 'Object ID' and 'Century')
+Y = array[:, -1]    # Target variable (last column, 'Century')
+test_size = 0.1
+seed = 7
 
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
